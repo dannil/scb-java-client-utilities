73799505c8710364506f70f84b7d4a77bae1e801
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-03-08 22:47:55 +0100

    Added test updates

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,18 +1,17 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.dannil.scbjavaclient.client.AbstractClient;
 import com.github.dannil.scbjavaclient.utility.JsonUtility;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Test {
 
@@ -39,65 +38,55 @@ public class Test {
 
 	}
 
-	public static List<Entry> findChildren() throws InterruptedException {
-		System.out.println("findChildren()");
-
-		// List<Entry> lst = findChildren("BE/");
-		List<Entry> lst = findChildren("");
-		lst.removeAll(Collections.singleton(null));
-
-		return lst;
-	}
+	// public static List<Entry> findChildren() throws InterruptedException {
+	// System.out.println("findChildren()");
+	//
+	// List<Entry> lst = findChildren("BE/");
+	// // List<Entry> lst = findChildren("");
+	// lst.removeAll(Collections.singleton(null));
+	//
+	// return lst;
+	// }
 
 	public static List<Entry> findChildren(String currentAddress) throws InterruptedException {
 		System.out.println("findChildren(String): calling findChildren(String) with address " + currentAddress);
 
 		String response = client.get(currentAddress);
-
 		JsonNode fetched = JsonUtility.getNode(response);
 
 		List<Entry> nodes = new ArrayList<>();
-
 		Iterator<JsonNode> it = fetched.iterator();
 		while (it.hasNext()) {
 			JsonNode next = it.next();
 			if (next.has("id")) {
-				System.out.println("Node has id");
+				Thread.sleep(1000);
 
 				Entry e = new Entry();
 				e.setId(next.get("id").asText());
 				e.setText(next.get("text").asText());
 
-				Thread.sleep(1000);
-
 				nodes.add(e);
-				List<Entry> lst = findChildren(currentAddress + next.get("id").asText() + "/");
-				// System.out.println("Nodes: " + nodes);
-				if (lst.size() != 0) {
-					e.addChildren(lst);
+				List<Entry> children = findChildren(currentAddress + next.get("id").asText() + "/");
+				if (children.size() != 0) {
+					e.addChildren(children);
 				}
-
 			}
 		}
-		// System.out.println(nodes);
 		return nodes;
 	}
 
-	public static void main(String[] args) throws InterruptedException, JsonProcessingException, FileNotFoundException,
-			UnsupportedEncodingException {
-
-		List<Entry> children = findChildren();
+	public static void main(String[] args) throws InterruptedException, IOException {
+		List<Entry> children = findChildren("BE/");
 		System.out.println("Final list: " + children);
 
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.setSerializationInclusion(Include.NON_NULL);
-		JsonNode res = mapper.convertValue(children, JsonNode.class);
-		System.out.println(res);
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-		String indented = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(children);
-		PrintWriter writer = new PrintWriter("scb.json", "UTF-8");
-		writer.print(indented);
-		writer.close();
+		File file = new File("scb.json");
+		try (FileWriter fw = new FileWriter(file.getAbsoluteFile())) {
+			try (BufferedWriter bw = new BufferedWriter(fw)) {
+				String content = gson.toJson(children, List.class);
+				bw.write(content);
+			}
+		}
 	}
-
 }
