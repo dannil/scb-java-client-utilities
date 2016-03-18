bb90a48540ef9e4a421e46df6955de96b2ca115e
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-03-18 13:31:35 +0100

    Renamed test method

---
+++ src/main/java/TestChangeUrlLocale.java
@@ -0,0 +1,18 @@
+import com.github.dannil.scbjavaclient.utility.URLUtility;
+
+public class TestChangeUrlLocale {
+
+	public static void main(String[] args) {
+		String url = URLUtility
+				.changeUrlLocale("http://api.scb.se/OV0104/v1/doris/en/ssd/BE/BE0101/BE0101A/BefolkningNy");
+
+		String expected = "http://api.scb.se/OV0104/v1/doris/sv/ssd/BE/BE0101/BE0101A/BefolkningNy";
+
+		System.out.println(url);
+		System.out.println(expected);
+
+		if (!url.equals(expected)) {
+			throw new AssertionError(url + " != " + expected);
+		}
+	}
+}
