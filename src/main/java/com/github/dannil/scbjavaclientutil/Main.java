c80b8e0e4170d1196ebe5a2eb3c04a4a3d0e4dae
Daniel Nilsson <daniel.nilsson94@outlook.com>
2016-10-25 21:48:27 +0200

    Modified main method

--- src/main/java/com/github/dannil/scbjavaclientutil/Main.java
+++ src/main/java/com/github/dannil/scbjavaclientutil/Main.java
@@ -2,17 +2,22 @@ package com.github.dannil.scbjavaclientutil;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 
+import com.github.dannil.scbjavaclientutil.contents.SCBTableStatistics;
 import com.github.dannil.scbjavaclientutil.contents.SCBTableValues;
 import com.github.dannil.scbjavaclientutil.contents.SCBTreeStructure;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-		SCBTreeStructure c = new SCBTreeStructure();
-
 		String table = "TK";
+
+		File f = new File("values");
+		File statisticsDestination = new File("statistics");
+
+		SCBTreeStructure c = new SCBTreeStructure();
+		SCBTableValues v = new SCBTableValues(f);
+		SCBTableStatistics s = new SCBTableStatistics(statisticsDestination);
 
 		// DateTime before = DateTime.now();
 		// List<Entry> children = c.getTableOfContents(table);
@@ -20,19 +25,19 @@ public class Main {
 		//
 		// c.generateFile(table, before, after, children);
 
-		File f = new File("values");
-
 		// Entry rootChild = new Entry();
 		// rootChild.setId(table);
 		// rootChild.addChildren(children);
 
-		SCBTableValues v = new SCBTableValues(f);
 		// v.getValues("", rootChild);
 
-		File jsonLocation = new File("scb_2016-10-24T17-49-41.124_2016-10-24T19-25-10.077.json");
-		String json = new String(Files.readAllBytes(jsonLocation.toPath()));
+		// File jsonLocation = new File("scb_2016-10-24T17-49-41.124_2016-10-24T19-25-10.077.json");
+		// String json = new String(Files.readAllBytes(jsonLocation.toPath()));
+		//
+		// v.getValues("", json);
 
-		v.getValues("", json);
+		File f3 = new File("values_2016-10-24T21-14-03.307");
+		s.getStatistics(f3);
 
 		System.out.println("Done!");
 	}
