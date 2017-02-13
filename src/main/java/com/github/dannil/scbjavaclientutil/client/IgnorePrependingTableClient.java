71942777d8fba61d8a73c489201b434d2c0db85a
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-02-13 19:50:17 +0100

    Updated scb-java-client dependency

--- src/main/java/com/github/dannil/scbjavaclientutil/client/IgnorePrependingTableClient.java
+++ src/main/java/com/github/dannil/scbjavaclientutil/client/IgnorePrependingTableClient.java
@@ -6,22 +6,28 @@ import com.github.dannil.scbjavaclient.client.AbstractClient;
 
 public class IgnorePrependingTableClient extends AbstractClient {
 
-	public IgnorePrependingTableClient() {
-		super();
-	}
+    public IgnorePrependingTableClient() {
+        super();
+    }
 
-	public IgnorePrependingTableClient(Locale locale) {
-		super(locale);
-	}
+    public IgnorePrependingTableClient(Locale locale) {
+        super(locale);
+    }
 
-	@Override
-	public String get(String url) {
-		return super.get(url);
-	}
+    @Override
+    public String get(String url) {
+        return super.get(url);
+    }
 
-	@Override
-	public String post(String url, String query) {
-		return super.post(url, query);
-	}
+    @Override
+    public String post(String url, String query) {
+        return super.post(url, query);
+    }
+
+    @Override
+    public String getUrl() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
