5ffd3bb98883c56fb4f4c21ca9e3f20f950e107d
Daniel Nilsson <daniel.nilsson94@outlook.com>
2017-03-18 09:24:23 +0100

    Updated logic

--- src/main/java/com/github/dannil/scbjavaclientutil/Test.java
+++ src/main/java/com/github/dannil/scbjavaclientutil/Test.java
@@ -1,48 +1,116 @@
 package com.github.dannil.scbjavaclientutil;
 
-import java.util.Locale;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.github.dannil.scbjavaclient.client.SCBClient;
+import com.github.dannil.scbjavaclient.client.population.amount.PopulationAmountClient;
+import com.github.dannil.scbjavaclient.client.population.demography.PopulationDemographyClient;
+import com.github.dannil.scbjavaclient.model.GenericModel;
+import com.github.dannil.scbjavaclient.model.environment.landandwaterarea.Area;
+import com.github.dannil.scbjavaclient.model.population.amount.Population;
+import com.github.dannil.scbjavaclient.model.population.demography.MeanAgeFirstChild;
+import com.github.dannil.scbjavaclient.model.population.density.Density;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// AbstractRequester req = RequesterFactory.getRequester(RequestMethod.GET);
-		//
-		// String json =
-		// req.getBodyAsStringFromTable("BE/BE0101/BE0101B/BefolkningMedelAlder");
-		// Map<String, List<String>> inputs = JsonUtility.getInputs(json);
-		//
-		// StringBuilder builder = new StringBuilder();
-		// for (Entry<String, List<String>> entry : inputs.entrySet()) {
-		// String key = entry.getKey();
-		// List<String> value = entry.getValue();
-		//
-		// builder.append(key);
-		// builder.append(": ");
-		//
-		// for (String s : value) {
-		// builder.append(s);
-		// builder.append(", ");
-		// }
-		// }
-		// System.out.println(builder.toString());
+    public static void main(String[] args) {
+        // Create the client
+        SCBClient client = new SCBClient();
 
-		SCBClient c = new SCBClient();
-		System.out.println(SCBClient.isSupportedLocale(new Locale("fr", "FR")));
+        // Retrieve some client(s) matching the table(s) you want to fetch information
+        // from
+        PopulationDemographyClient populationDemographyClient = client.population().demography();
+        PopulationAmountClient populationAmountClient = client.population().amount();
 
-		// Map<String, Collection<?>> inputs = new HashMap<String, Collection<?>>();
-		// inputs.put("ContentsCode", Arrays.asList("BE0101N1", "BE0101N2"));
-		// inputs.put("Region", Arrays.asList("0114"));
-		// inputs.put("Civilstand", Arrays.asList("OG"));
-		// inputs.put("Alder", Arrays.asList(45, 50, 52));
-		// inputs.put("Kon", null);
-		// inputs.put("Tid", Arrays.asList(2011));
-		//
-		// String raw = c.getRawData("BE/BE0101/BE0101A/BefolkningNy/", inputs);
-		// String conventional = JsonUtility.toConventionalJson(raw).toString();
-		//
-		// System.out.println(raw);
-		// System.out.println(conventional);
-	}
+        // Retrieve all population statistics
+        List<Population> populationData = populationAmountClient.getPopulation();
+
+        // Retrieve all mean age for first child birth statistics
+        List<MeanAgeFirstChild> firstChildData = populationDemographyClient.getMeanAgeFirstChild();
+
+        // You may also want to skip the explicit creation of the matching client and
+        // fetch data
+        // directly from the method calls.
+        List<Density> densityData = client.population().density().getDensity();
+
+        // Specify the criterion for the information we want to retrieve
+        List<String> regions = Arrays.asList("00", "01", "0114");
+        List<String> types = Arrays.asList("01", "02");
+
+        // Defining an input as null means that we want to retrieve all information
+        // for this input.
+        //
+        // Example: if the information from the API specifies that it has information
+        // for the years 2011 through 2015 (for a specific table), the inputs underneath
+        // this row would result in an equivalent response from the API.
+        //
+        // List<Integer> years = Arrays.asList(2011, 2012, 2013, 2014, 2015);
+        // List<Integer> years = null;
+        //
+        // This makes it easy to be selective when we have a case where we
+        // want to retrieve all information for one input and at the same time
+        // restrict the input for another (for example the years 2012 and 2014).
+        List<Integer> years = null;
+
+        // Retrieve all area statistics using the selected values
+        List<Area> areas = client.environment().landAndWaterArea().getArea(regions, types, years);
+
+        // Specify the table to retrieve from. The response will be a JSON string
+        // containing all the
+        // available data from our specified table.
+        String json = client.getRawData("BE/BE0101/BE0101A/BefolkningNy");
+
+        // Fetch all available inputs for a table. Every key in the map corresponds to an
+        // available input
+        // parameter and the values for a specific key are all the available values for a
+        // specific input.
+        // The contents of availableInputs can then be analyzed and used for fetching more
+        // specific data.
+        Map<String, Collection<String>> availableInputs = client.getInputs("BE/BE0101/BE0101A/BefolkningNy");
+
+        // Specifies the criterion for the information we want to retrieve, in this case:
+        // The contents code BE0101N1 (the total population, so the API knows what
+        // information we want)
+        // The regions 00 (whole country), 01 (Stockholm County) and 0114 (Upplands Väsby)
+        // The relationship statuses OG (unmarried) and G (married)
+        // The ages 45 and 50
+        // The genders are null; we want to retrieve information for all genders
+        // The years 2011 and 2012
+        Map<String, Collection<?>> inputs = new HashMap<>();
+        inputs.put("ContentsCode", Arrays.asList("BE0101N1"));
+        inputs.put("Region", Arrays.asList("00", "01", "0114"));
+        inputs.put("Civilstand", Arrays.asList("OG", "G"));
+        inputs.put("Alder", Arrays.asList(45, 50));
+        inputs.put("Kon", null);
+        inputs.put("Tid", Arrays.asList(2011, 2012));
+
+        // Specify the table to retrieve from and our inputs to this table. The response
+        // will be a JSON
+        // string containing the data that matched our criterion.
+        json = client.getRawData("BE/BE0101/BE0101A/BefolkningNy", inputs);
+
+        // Fetch all data from a table and construct a GenericModel using this data.
+        json = client.getRawData("BE/BE0101/BE0101A/BefolkningNy");
+        GenericModel m = new GenericModel(json);
+
+        // Retrieve all the entries into a collection.
+        Collection<Map<String, Object>> entries = m.getEntries();
+
+        // Retrieve all the entries having the code "alder" with either value 45 or 50 and
+        // the code
+        // "region" with value 01.
+        Map<String, Collection<String>> inputs2 = new HashMap<>();
+        inputs2.put("alder", Arrays.asList("45", "50"));
+        inputs2.put("region", Arrays.asList("01"));
+
+        entries = m.getEntries(inputs2);
+
+        // Retrieve all the entries having the code "alder" with value 45.
+        entries = m.getEntries("alder", "45");
+
+    }
 }
--- src/main/java/com/github/dannil/scbjavaclientutil/contents/SCBTreeStructure.java
+++ src/main/java/com/github/dannil/scbjavaclientutil/contents/SCBTreeStructure.java
@@ -2,10 +2,13 @@ package com.github.dannil.scbjavaclientutil.contents;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.dannil.scbjavaclient.exception.SCBClientException;
@@ -13,6 +16,9 @@ import com.github.dannil.scbjavaclient.format.json.JsonConverter;
 import com.github.dannil.scbjavaclientutil.client.IgnorePrependingTableClient;
 import com.github.dannil.scbjavaclientutil.files.FileUtility;
 import com.github.dannil.scbjavaclientutil.model.Entry;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 import org.joda.time.DateTime;
 import org.joda.time.Duration;
@@ -84,6 +90,39 @@ public class SCBTreeStructure {
         System.out.println("Writing " + builder.toString());
         File file = new File(builder.toString());
         FileUtility.writeToSystem(file, entries);
+    }
+
+    public Map<String, Integer> getLevels(File file) throws IOException {
+        String json = new String(Files.readAllBytes(file.toPath()));
+
+        Gson gson = new GsonBuilder().create();
+
+        List<Entry> entries = gson.fromJson(json, new TypeToken<List<Entry>>() {
+        }.getType());
+
+        Entry rootChild = new Entry();
+        rootChild.setId("");
+        rootChild.addChildren(entries);
+
+        return getLevels(rootChild, "", 0);
+    }
+
+    private Map<String, Integer> getLevels(Entry entry, String previousTable, int level) throws IOException {
+        Map<String, Integer> map = new HashMap<>();
+
+        if (entry.getChildren() != null) {
+            for (Entry e : entry.getChildren()) {
+                String altered = previousTable;
+                if (previousTable.length() > 0) {
+                    altered = previousTable + "/";
+                }
+                map.putAll(getLevels(e, altered + e.getId(), level + 1));
+            }
+        } else {
+            map.put(previousTable, level);
+        }
+
+        return map;
     }
 
 }
